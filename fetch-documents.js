80138bd6cceb3b456e7d2bed08d1d60f8c8aa53f
Julien Wajsberg <felash@gmail.com>
2023-01-06 18:34:59 +0100

    Make the script more generic

--- fetch-documents.js
+++ fetch-documents.js
@@ -3,17 +3,24 @@ import path from "node:path";
 import { convertFromAssoconnectFormat } from "./import/assoconnect.js";
 
 function extractUrlFromAssoconnectField(input) {
-  const startUrl = input.lastIndexOf(" - ");
+  const startUrl = input.lastIndexOf("https:");
   if (startUrl < 0) {
     return null;
   }
 
   const startType = input.lastIndexOf(".", startUrl);
-  const type = startType < 0 ? "xxx" : input.slice(startType + 1, startUrl);
+  if (startType < 0) {
+    return null;
+  }
+  const endType = input.indexOf(" ", startType);
+  if (endType < 0) {
+    return null;
+  }
+  const type = input.slice(startType + 1, endType);
 
   return {
     type,
-    url: input.slice(startUrl + 3).replaceAll("&amp;", "&"),
+    url: input.slice(startUrl).replaceAll("&amp;", "&"),
   };
 }
 
@@ -29,6 +36,26 @@ async function fetchAndSave({ nom, prenom, urlInformation, dir, what }) {
     const buffer = await response.arrayBuffer();
     await writeFile(filename, new DataView(buffer));
     console.log(`${what} for ${nom} ${prenom} saved in ${filename}`);
+  }
+}
+
+async function extractPropertieswithUrl(participant) {
+  const nom = participant.Nom;
+  const prenom = participant.Prénom;
+
+  for (const [property, value] of Object.entries(participant)) {
+    const urlInformation = extractUrlFromAssoconnectField(value);
+    if (urlInformation) {
+      const dir = path.join(directoryToWrite, property);
+      await mkdir(dir, { recursive: true });
+      await fetchAndSave({
+        dir,
+        nom,
+        prenom,
+        urlInformation,
+        what: property,
+      });
+    }
   }
 }
 
@@ -72,41 +99,11 @@ if (!directoryToWrite) {
   process.exit(-1);
 }
 
-const caciDir = path.join(directoryToWrite, "caci");
-const diplomeDir = path.join(directoryToWrite, "diplome");
-await mkdir(caciDir, { recursive: true });
-await mkdir(diplomeDir, { recursive: true });
-
 const rawData = await readFile(fileToRead, { encoding: "utf-8" });
 const data = await convertFromAssoconnectFormat(rawData);
 
 const promises = data.map(async (participant) => {
-  const nom = participant.Nom;
-  const prenom = participant.Prénom;
-  const caciInformation = participant["CACI 2022-2023"];
-  const diplomeInformation = participant["Scan des diplômes 2022-2023"];
-
-  const caciUrl = extractUrlFromAssoconnectField(caciInformation);
-  const diplomeUrl = extractUrlFromAssoconnectField(diplomeInformation);
-
-  if (caciUrl) {
-    await fetchAndSave({
-      dir: caciDir,
-      nom,
-      prenom,
-      urlInformation: caciUrl,
-      what: "CACI",
-    });
-  }
-  if (diplomeUrl) {
-    await fetchAndSave({
-      dir: diplomeDir,
-      nom,
-      prenom,
-      urlInformation: diplomeUrl,
-      what: "diplome",
-    });
-  }
+  await extractPropertieswithUrl(participant);
 });
 
 await Promise.all(promises);
